4d0f4e1933172b9e9c322d4a5eeeee229de5fd3b
David Frankel <david.frankel@wustl.edu>
2018-11-08 15:20:52 -0600

    Added functioning delete button

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var todos = JSON.parse(this.responseText);
         for (var i = 0; i < todos.length; i++) {
-          namesHtml += '<li'+ (todos[i]['completed'] ? ' class="completed"' : '') +'><span class="listItemContent" id="'+ todos[i]['id'] +'" onclick="flipCompleted(this);">' + todos[i]['text'] + '</span><button type="button" name="deleteTodo">X</button></li>';
+          namesHtml += '<li'+ (todos[i]['completed'] ? ' class="completed"' : '') +'><span class="listItemContent" id="'+ todos[i]['id'] +'" onclick="flipCompleted(this);">' + todos[i]['text'] + '</span><button type="button" name="deleteTodo" onclick="deleteTodo(this);">X</button></li>';
         }
         document.getElementById("itemList").innerHTML = namesHtml;
     }
@@ -26,6 +26,20 @@ function flipCompleted(ele) {
   xhttp.setRequestHeader("x-api-key", "66d24650014ef29878e637f3b1e42641eee0f334d21ecd8a6aa518ba2c1ce37b");
   var data = {completed: (document.getElementById(ele.id).parentElement.classList.contains("completed") ? false : true)};
   xhttp.send(JSON.stringify(data));
+}
+function deleteTodo(ele) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById(ele.previousSibling.id).parentElement.remove();
+    } else if (this.readyState == 4) { //Error
+      console.log(this.responseText);
+    }
+  };
+  xhttp.open("DELETE", "https://api.kraigh.net/todos/" + ele.previousSibling.id);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.setRequestHeader("x-api-key", "66d24650014ef29878e637f3b1e42641eee0f334d21ecd8a6aa518ba2c1ce37b");
+  xhttp.send();
 }
 // var xhttp = new XMLHttpRequest();
 // xhttp.onreadystatechange = function() {
