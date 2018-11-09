d365d42478e23547718531e54bcc3e5fa65e5ac3
David Frankel <david.frankel@wustl.edu>
2018-11-08 21:40:26 -0600

    Updated styling and improved function

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function refreshTodoList() {
       if (this.readyState == 4 && this.status == 200) {
           var todos = JSON.parse(this.responseText);
           for (var i = 0; i < todos.length; i++) {
-            namesHtml += '<li'+ (todos[i]['completed'] ? ' class="completed"' : '') +'><span class="listItemContent" id="'+ todos[i]['id'] +'" onclick="flipCompleted(this);">' + todos[i]['text'] + '</span><button type="button" name="deleteTodo" onclick="deleteTodo(this);">X</button></li>';
+            namesHtml += '<li onclick="flipCompleted(this);" '+ (todos[i]['completed'] ? ' class="completed"' : '') +'><span class="listItemContent" id="'+ todos[i]['id'] +'">' + todos[i]['text'] + '</span><button type="button" name="deleteTodo" onclick="deleteTodo(event, this);">X</button></li>';
           }
           document.getElementById("itemList").innerHTML = namesHtml;
       }
@@ -21,19 +21,21 @@ function flipCompleted(ele) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById(ele.id).parentElement.classList.toggle("completed");
+      ele.classList.toggle("completed");
     } else if (this.readyState == 4) { //Error
       console.log(this.responseText);
     }
   };
-  xhttp.open("PUT", "https://api.kraigh.net/todos/" + ele.id);
+  xhttp.open("PUT", "https://api.kraigh.net/todos/" + ele.childNodes["0"].id);
   xhttp.setRequestHeader("Content-type", "application/json");
   xhttp.setRequestHeader("x-api-key", "66d24650014ef29878e637f3b1e42641eee0f334d21ecd8a6aa518ba2c1ce37b");
-  var data = {completed: (document.getElementById(ele.id).parentElement.classList.contains("completed") ? false : true)};
+  var data = {completed: (ele.classList.contains("completed") ? false : true)};
   xhttp.send(JSON.stringify(data));
 }
 
-function deleteTodo(ele) {
+function deleteTodo(event, ele) {
+  event.stopPropagation(); //Used to prevent li onclick function from running
+  event.preventDefault();
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
